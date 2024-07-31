5c095d8423ec1bcf202cfe229a245e23c0cccdd2
David Quiceno <davidquiceno69@gmail.com>
2024-07-30 19:38:18 -0500

    Par o Inpar

---
+++ Challenge-01.js
@@ -0,0 +1,22 @@
+function regular(x) {
+    if (x % 2 === 0) {
+      console.log(x + " es par");
+    } else {
+      console.log(x + " es impar");
+    }
+  }
+  
+regular(4);
+regular(5);
+
+const flecha = (x) => {
+    if (x % 2 === 0) {
+      console.log(x + " es par");
+    } else {
+      console.log(x + " es impar");
+    }
+  };
+  
+flecha(4);
+flecha(5);
+  
