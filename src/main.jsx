ff66ac9778c12059bde7fdf8275d36eca42f7c2a
David Quiceno <davidquiceno69@gmail.com>
2024-10-12 14:43:51 -0500

    Chall4

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FirstApp from './components/Challenge2'
+import {GifGrid} from './components/Challenge4-GifGrid'
 import './index.css'
 import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FirstApp />
+    <GifGrid category={category}/>
   </StrictMode>,
 )
