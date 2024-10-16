cba9ddf5f13d68db85d6a0da57498ff9070084d6
David Quiceno <davidquiceno69@gmail.com>
2024-10-16 16:11:55 -0500

    Chall9

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 //import FirstApp from './FirstApp'
 import './index.css'
 import "./App.css"
-import { MultipleCustomHooks } from './components/Challenge7';
+
+import { TodoAdd } from './components/TodoAdd';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks/>
+    <TodoAdd/>
   </React.StrictMode>,
 )
