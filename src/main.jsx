3aeeb033e25832777bd5618b2d8fdb2e0bdb55ae
David Quiceno <davidquiceno69@gmail.com>
2024-11-13 16:54:25 -0500

    cambios finales

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
-import "./App.css"
-import { MultipleCustomHooks } from './components/MultipleCustomHooks';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MultipleCustomHooks/>
-  </React.StrictMode>,
-)
+import { Provider } from 'react-redux';
+import store from './store/pokemonStore';
+
+import './index.css';
+import './App.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- src/store/pokemonStore.js
+++ src/store/pokemonStore.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todosReducer from "./todosSlice";
 import pokemonReducer from "./pokemonSlice";
 
 const store = configureStore({
   reducer: {
-    todos: todosReducer,
     pokemon: pokemonReducer,
   },
 });
